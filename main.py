841e3cadb3d9c5b572491688f1b34a946aec6d0b
nnyborg <nny@live.dk>
2021-06-08 12:34:18 +0200

    fix main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 # bruges til at kopiere filer i full backup og differential backup
 import shutil
 # bruges til at skrive timestamps i log
-from datetime import datetime
+import datetime
 # bruges til at sammenligne directories/filer, i differential backup
 import filecmp
 
